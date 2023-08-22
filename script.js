85b1ea677fe94e7fd9fca5ba6db658088d9d2002
purrrplelipton <61193553+purrrplelipton@users.noreply.github.com>
2023-08-22 02:08:48 +0100

    gyattt

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const navList = document.getElementById('navlist');
-const navLinks = navList.getElementsByTagName('a');
 const showNav = document.getElementById('show-nav');
 const hideNav = document.getElementById('hide-nav');
+const navList = document.querySelector('.navlist__bar');
+const navLinks = navList.querySelectorAll('a');
+const cuboid = document.querySelector('.cuboid');
+const faces = cuboid.querySelectorAll('.face');
 
 function hideNavBar() {
   if (!navList.classList.contains('hidden')) {
@@ -19,3 +21,10 @@ hideNav.addEventListener('click', hideNavBar);
 for (const link of navLinks) {
   link.addEventListener('click', hideNavBar);
 }
+
+let angle = 0;
+
+setInterval(() => {
+  cuboid.style.transform = `rotateX(${angle}deg)`;
+  angle += 90;
+}, 5375);
